f868d299a34a868e9dea2bc31d52edd355a7adb8
JEVG <iesu9011@gmail.com>
2025-03-26 23:27:31 -0500

    adding the db to csv func

--- tools/csv_generator.py
+++ tools/csv_generator.py
@@ -1,7 +1,13 @@
 from dbmanager import get_expenses
 import csv
 
-csv_path = ''
-
-def generate_csv():
-    pass
+def generate_csv(file_path):
+    """"""
+    expenses= get_expenses()
+    if not expenses:
+        return 0
+    with open(file_path,mode='w',newline='',encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=["id", "description", "amount", "date", "category"])
+        writer.writeheader()
+        writer.writerows(expenses)
+    return 1
